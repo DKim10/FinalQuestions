b7267deb05749a58c0bb75b75c6d0ed7c806cc37
Daniel Kim <dankim@udel.edu>
2015-12-18 16:19:41 -0500

    commit

--- FinalExamQuestions/src/QuestionA/Person.java
+++ FinalExamQuestions/src/QuestionA/Person.java
@@ -6,7 +6,7 @@ import java.util.Date;
 /*
  * comment
  */
-public abstract class Person {
+public abstract class Person implements iPersonRead, iPersonWrite{
 
 	private Date DOB;
 	private String FirstName;
---
+++ FinalExamQuestions/src/QuestionA/iPersonRead.java
@@ -0,0 +1,20 @@
+package QuestionA;
+
+import java.util.Date;
+ 
+public interface iPersonRead {
+
+	String getFirstName(String FirstName);
+	
+	 String getMiddleName(String MidleName);
+	
+	String getLastName(String LastName);
+	
+	 Date getDOB(Date DOB);
+	
+	String getAddress(String Address); 
+
+	String getPhone(String Phone);
+	
+	 String getEmail(String Email);
+}
